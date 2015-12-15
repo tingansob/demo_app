ee0f7958c8e6519b844f325b44e37dd57c0161a6
Ishmael BabaGaia <tingansob@gmail.com>
2015-12-15 07:17:15 -0500

    added navi at top of page as well as edit and delete functions to each page. RAILS IS GREAT!

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+	before_action :find_post, only: [:show, :edit, :update, :destroy]
+
   def index
   	@post = Post.all.order("created_at DESC")
   end
@@ -18,10 +20,30 @@ class PostsController < ApplicationController
   end
  
   def show
-  	@post = Post.find(params[:id])
   end
 
+  def edit
+  end
+
+  def update
+   	if @post.update(post_params)
+   		redirect_to @post
+   	else
+   		render 'edit'
+   	end
+  end
+  def destroy
+  	@post.destroy
+  	redirect_to root_path
+  end
+
+
+
   private
+
+  def find_post
+  	   	@post = Post.find(params[:id])
+  end
 
   def post_params
 	params.require(:post).permit(:title, :body)
